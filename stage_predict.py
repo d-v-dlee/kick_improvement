0f4740a51bdd62e1e1d99c8f6a8b2d7b0a8974f9
Emily Karboski <eskarboski@gmail.com>
2018-11-12 20:25:36 -0800

    Update stage_predict.py with better comments

--- stage_predict.py
+++ stage_predict.py
@@ -14,24 +14,29 @@ def stage_score_plot(estimator, X_train, y_train, X_test, y_test):
     Returns: A plot of the number of iterations vs the MSE for the model for
     both the training set and test set.
     '''
+    # fit estimator
     estimator.fit(X_train, y_train)
     train_mse_at_stages = []
     test_mse_at_stages = []
     
+    # iterate through all stages for test and train and record mean_square_errors lists
     for y1, y2 in zip(estimator.staged_predict(X_train), estimator.staged_predict(X_test)):
         train_mse = mean_squared_error(y_train, y1)
         train_mse_at_stages.append(train_mse)
         
         test_mse = mean_squared_error(y_test, y2)
         test_mse_at_stages.append(test_mse)
-        
-    xs = range(0, len(test_mse_at_stages))
+
+    # find the # of trees at which test error is the lowest
     lowest_test_error = np.argmin(test_mse_at_stages)
 
+    # create xs in order to plot. each x represents n_estimators.
+    xs = range(0, len(test_mse_at_stages))
+
     ax.plot(xs, train_mse_at_stages, 
-            label="{} Train - :learning rate {}".format(estimator.__class__.__name__, estimator.learning_rate))
+            label="{} Train".format(estimator.__class__.__name__)
     ax.plot(xs, test_mse_at_stages, 
-            label="{} Test - :learning rate {}".format(estimator.__class__.__name__, estimator.learning_rate))
+            label="{} Test".format(estimator.__class__.__name__)
     ax.axvline(lowest_test_error)
     print(lowest_test_error)
 
